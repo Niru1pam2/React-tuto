7dd3e57c063dc1d8ea9dad6254a18070c5a7814a
Niru1pam2 <nirupamtalukdar123@gmail.com>
2024-02-02 18:10:47 +0530

    first commit

---
+++ src/books.js
@@ -0,0 +1,31 @@
+
+
+import img2 from './images/img2.jpg'
+import img3 from './images/img3.jpg'
+import img4 from './images/img4.jpg'
+
+
+
+export const books = [
+  {
+    author : 'Chetan Bhagat',
+    title : '11 Rules For Life: Secrets to Level Up',
+    img : img2,
+    id: 1,
+  },
+  {
+    author : 'Joseph Murphy',
+    title : 'The Power of Your Subconscious Mind: Original',
+    img : img3,
+    id: 2,
+  },
+  {
+    author : 'M Laxmikanth',
+    title : 'Indian Polity for UPSC',
+    img : img4,
+    id: 3,
+  }
+]
+
+
+
